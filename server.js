cadfc2139ddcc459bb03ec565e8fb5a684a3afb1
sathya53 <30333766+sathya53@users.noreply.github.com>
2017-08-09 08:22:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function (req, res) {
+  res.send('This the call to Article One');
+});
+
+app.get('/article-two', function (req, res) {
+  res.send('This the call to Article Two');
+});
+
+app.get('/article-three', function (req, res) {
+  res.send('This the call to Article Three');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
