aabb7d51468d7e48b002748e8ea22a06c4eb17c4
sathya53 <30333766+sathya53@users.noreply.github.com>
2017-08-23 07:46:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var pool = require('pg').pool;
-var config=(
+var config={
     user: 'sathyanarayananmail',
     database: 'sathyanarayananmail',
-    host:'sathyanarayananmail',
+    host:'db.imad.hasura-app.io',
     port:'5432',
     password:process.env.DB_PASSWORD
-    );
+    };
 
 
 var app = express();
@@ -22,9 +22,9 @@ app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
-var pool= new pool(config);
-app.get('/test-db', function (req, res) {
- pool.query('select * from employee',function(err,result)) {
+var pool= new Pool(config);
+app.get('/test-db', function (req, res){ 
+ pool.query('select * from employee',function(err,result) {
 
     if (err){
     res.status(500).send(err.tostring())
