c5c31c53bcae6e969c199ff677385beac5312ede
sathya53 <30333766+sathya53@users.noreply.github.com>
2017-08-23 08:06:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config={
     database: 'sathyanarayananmail',
     host:'db.imad.hasura-app.io',
     port:'5432',
-    password:process.env.DB_PASSWORD
+    password:'db-sathyanarayananmail-63696'
     };
 
 
